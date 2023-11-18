f836ec085aed8c6bb4a4d3b1317c7a0b99b8d407
michaelvelasco000 <102225357+michaelvelasco000@users.noreply.github.com>
2023-11-18 13:01:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const url = `https://animechan.xyz/api/random/anime?title=naruto`;
+
+function App() {
+  const [quote, setQuote] = useState("");
+  const [anime, setAnime] = useState("");
+  const [character, setCharacter] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        setQuote(json.quote);
+        setAnime(json.anime);
+        setCharacter(json.character);
+      } catch (error) {
+        // Handle errors here if necessary
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <p>Hello</p>
+      <p>anime: {anime}</p>
+      <p>Character: {character}</p>
+      <p>Quote: {quote}</p>
+      <button onClick={() => window.location.reload(false)}>reload</button>
+    </>
+  );
+}
+
+export default App;
